1bf9fe0fc92d45ac988b092138dd1ddd6acbdf6c
Freja Bermann <frejantbermann@gmail.com>
2024-09-25 14:25:05 +0200

    Added functionality for voting, checking if citizen has voted or can vote and if a referendum is open. Some names are temporary. Freja

--- src/main/java/agile18/demo/model/Database.java
+++ src/main/java/agile18/demo/model/Database.java
@@ -87,7 +87,90 @@ public class Database {
         jdbc.execute(sql);
     }
 
-// --------------------- TEST ---------------------
+    /**
+     * Returns true if the given referendum is currently open.
+     * @param id id for referendum.
+     */
+    public boolean referendumOpen(String id){
+        String sql = "SELECT 1 FROM Referendum " +
+                "WHERE id = '" + id + "'" +
+                "AND CURRENT_DATE >= startDate\n" +
+                "AND CURRENT_DATE <= endDate;";
+        return !jdbc.queryForList(sql).isEmpty();
+    }
+
+    /**
+     * Tries to let given citizen vote in given referendum.
+     * @param citizenId id for citizen.
+     * @param referendumId id for referendum.
+     * @param vote What kind of vote.
+     * @return true if vote is successful, false otherwise.
+     */
+    public boolean citizenVote(String citizenId, String referendumId, VoteEnum vote) {
+        if (!referendumOpen(referendumId) || !canCitizenVote(citizenId, referendumId)){
+            return false;
+        }
+        String sql;
+
+        if (vote == VoteEnum.FAVOR) {
+            sql = "UPDATE RefResults \n" +
+                    "SET favor = favor + 1 \n" +
+                    "WHERE referendum = '" + referendumId +"';";
+            jdbc.update(sql);
+        }
+        if (vote == VoteEnum.AGAINST) {
+            sql = "UPDATE RefResults \n" +
+                    "SET against = against + 1 \n" +
+                    "WHERE referendum = '" + referendumId +"';";
+            jdbc.update(sql);
+
+        }
+        if (vote == VoteEnum.BLANK) {
+            sql = "UPDATE RefResults \n" +
+                    "SET blank = blank + 1 \n" +
+                    "WHERE referendum = '" + referendumId +"';";
+            jdbc.update(sql);
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns true if citizen is eligible to vote in given referendum.
+     * @param citizenId id for citizen.
+     * @param referendumId id for referendum.
+     */
+    public boolean canCitizenVote(String citizenId, String referendumId){
+        String sql = "\n" +
+                "SELECT * FROM Citizens JOIN\n" +
+                "(SELECT name FROM Area JOIN Referendum ON Area.name = Referendum.area\n" +
+                "WHERE Referendum.id = '"+ referendumId +"'\n" +
+                "UNION\n" +
+                "SELECT name FROM Area JOIN Referendum ON Area.partof = Referendum.area\n" +
+                "WHERE Referendum.id = '" + referendumId + "')\n" +
+                "AS Valid\n" +
+                "WHERE Citizens.home = Valid.name\n" +
+                "AND Citizens.id NOT IN\n" +
+                "(Select citizen FROM RefRoll\n" +
+                "WHERE referendum = '" + referendumId + "') \n" +
+                "AND Citizens.id = '" + citizenId + "';";
+        return !jdbc.queryForList(sql).isEmpty();
+    }
+
+    /**
+     * Returns true if the citizen has voted in the given referendum.
+     * @param citizenId id for citizen.
+     * @param referendumId id for referendum.
+     */
+    public boolean hasCitizenVoted(String citizenId, String referendumId){
+        String sql = "SELECT 1 FROM RefRoll \n" +
+                "WHERE citizen = '" + citizenId + "' \n" +
+                "AND referendum = '" + referendumId + "';";
+        return !jdbc.queryForList(sql).isEmpty();
+    }
+
+
+    // --------------------- TEST ---------------------
     public void testReferendum(){
         String id = getUniqueRefId();
         System.out.println("id: "+ id);
--- src/main/resources/inserts.sql
+++ src/main/resources/inserts.sql
@@ -1,8 +1,51 @@
--- Mock data for us
--- Citizens
-INSERT INTO Citizens VALUES ('0305251111', 'Hassan Khan','hejsan123');
-INSERT INTO Citizens VALUES ('0311261111', 'David Golebiak', 'tjabatjena');
-INSERT INTO Citizens VALUES ('9603291111', 'Sebastian Kolbel', 'hejhallo');
 
 -- Area
-INSERT INTO Area (name,level) VALUES ('Göteborg','Municipal')
+INSERT INTO Area VALUES ('Göteborg','Municipal','Västra Götaland');
+INSERT INTO Area (name, level) VALUES ('Västra Götaland','County');
+INSERT INTO Area (name, level) VALUES ('Halland', 'County');
+INSERT INTO Area VALUES ('Varberg', 'Municipal', 'Halland');
+
+INSERT INTO Municipality VALUES ('Göteborg');
+INSERT INTO Municipality VALUES ('Varberg');
+
+-- Citizens
+INSERT INTO Citizens VALUES ('0305251111', 'Hassan Khan','hejsan123', 'Göteborg');
+INSERT INTO Citizens VALUES ('0311261111', 'David Golebiak', 'tjabatjena', 'Göteborg');
+INSERT INTO Citizens VALUES ('9603291111', 'Sebastian Kolbel', 'hejhallo', 'Varberg');
+
+-- Referendums
+INSERT INTO Referendum VALUES ('1', 'Västra Götaland', 'dance', 'dancing!', '2024-09-23', '2024-09-30');
+INSERT INTO Referendum VALUES ('2', 'Varberg', 'no dance', 'no dance plz', '2024-09-23', '2024-09-30');
+INSERT INTO Referendum VALUES ('3', 'Varberg', 'later', 'late', '2024-09-20', '2024-09-22');
+
+INSERT INTO RefResults VALUES ('1', 1, 0, 0);
+INSERT INTO RefResults VALUES ('2', 0, 0, 0);
+INSERT INTO RefResults VALUES ('3', 0, 0, 0);
+
+INSERT INTO RefRoll VALUES ('0305251111', '1');
+
+-- testing queries
+-- finds valid citizens for a given referendum
+SELECT * FROM Citizens JOIN
+(SELECT name FROM Area JOIN Referendum ON Area.name = Referendum.area
+WHERE Referendum.id = '1'
+UNION
+SELECT name FROM Area JOIN Referendum ON Area.partof = Referendum.area
+WHERE Referendum.id = '1')
+AS Valid
+WHERE Citizens.home = Valid.name
+AND Citizens.id NOT IN
+(Select citizen FROM RefRoll
+WHERE referendum = '1')
+AND Citizens.id = '9603291111';
+
+-- Checks if given citizen voted in referendum
+SELECT * FROM RefRoll
+WHERE citizen = '0305251111'
+AND referendum = '1';
+
+-- checks if referendum is open
+SELECT * FROM Referendum
+WHERE id = '1'
+AND CURRENT_DATE >= startDate
+AND CURRENT_DATE <= endDate;
